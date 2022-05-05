00fe90b4c12f5100688c974fb20613dbd351f91c
Madeline Prickette <madelineprickette@gmail.com>
2022-05-05 11:05:23 -0500

    Started Router in App.jsx

---
+++ src/components/App/CustomerForm/CustomerForm.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+function CustomerForm({}) {
+    return (
+        <section>
+            <h2>Enter Information</h2>
+        </section>
+    )
+}
+
+export default CustomerForm;
