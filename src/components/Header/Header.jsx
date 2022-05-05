a6ff1111d14ab769fc4b47445f147de6547aba17
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-05-05 10:35:11 -0500

    Created Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className='App-header'>
+            <h1 className='App-title'>Prime Pizza</h1>
+            <div className="cart">🛒</div>
+        </header>
+    )
+}
+
+export default Header;
