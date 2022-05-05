4c7c918dc48b403ac4d43b27764134b57d97a24b
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-05-05 13:22:10 -0500

    added pizzaList component

--- src/components/App/PizzaList/PizzaList.jsx
+++ src/components/App/PizzaList/PizzaList.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {useSelector} from 'react-redux'
 
 function PizzaList({ getPizzas }) {
-    const pizzaList = useSelector(store => store.pizzaList);
+    const pizzaList = useSelector(store => store.pizzaReducer);
 
   return (
-
-  );
+    <ul>
+      {pizzaList.map((pizza) => 
+        < PizzaItem key={index} pizza={pizza}/>  
+      )}
+    </ul>
+  )
 }
 
 export default PizzaList;
